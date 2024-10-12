cf2a10c827d4b8c8c21742391bdef121815bc027
dariodaddamio <dario.daddamio@gmail.com>
2024-10-12 14:03:23 -0700

    feat: starting to build the homepage

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,31 +1,15 @@
-import { useEffect, useState } from 'react';
+import Home from "./pages/homepage";
+import Admin from "./pages/admin";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
-  const [isLoading, setIsLoading] = useState(true);
-  const [error] = useState<Error | null>(null);
-  const [hello, setHello] = useState('');
-
-  useEffect(() => {
-    // Simulate an API call
-    setTimeout(() => {
-      setIsLoading(false);
-      setHello('Hello, World!');
-    }, 2000);
-
-    // Simulate an error
-    // setTimeout(() => {
-    //   setIsLoading(false);
-    //   setError(new Error('Something went wrong'));
-    // }, 2000);
-  }, []);
-
-  if (isLoading) return <div>Loading...</div>;
-  if (error) return <div>Error: {error.message}</div>;
-
   return (
-    <>
-      <div className="text-3xl font-bold text-blue-500">{hello}</div>
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/admin" element={<Admin />} />
+      </Routes>
+    </Router>
   );
 }
 
