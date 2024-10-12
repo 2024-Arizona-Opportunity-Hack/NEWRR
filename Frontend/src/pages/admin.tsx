2bee3ad6a45cf4d2e73fb58a41673a31ba893e93
dariodaddamio <dario.daddamio@gmail.com>
2024-10-12 14:03:23 -0700

    feat: starting to build the homepage

---
+++ Frontend/src/pages/admin.tsx
@@ -0,0 +1,13 @@
+// admin page with log in and powerbi platform later
+
+import React from "react";
+
+const Admin: React.FC = () => {
+  return (
+    <div>
+      <h1>Admin Page</h1>
+    </div>
+  );
+};
+
+export default Admin;
