b56c949cb0777cc765da0d3dd3d77a6e6d58273c
Ronald McInnes <rjmcars8@gmail.com>
2024-10-13 07:41:56 -0700

    fix: protected root routes

--- Frontend/src/pages/admin-dashboard.tsx
+++ Frontend/src/pages/admin-dashboard.tsx
@@ -2,8 +2,8 @@ import React from "react";
 import AnimalManagement from "../components/admin/AnimalManagament";
 import TaskManagement from "../components/admin/TaskManagement";
 import Navbar from "../components/Navbar";
-// import { useUser } from "../hooks/useUser";
-// import NotLoggedIn from "../components/ NotLoggedIn";
+import { useUser } from "../hooks/useUser";
+import NotLoggedIn from "../components/admin/NotLoggedIn";
 
 const AdminDashboard: React.FC = () => {
   const adminLinks = [
@@ -11,17 +11,17 @@ const AdminDashboard: React.FC = () => {
     { name: "Animal Dashboard", href: "/admin/animal-dashboard" },
     { name: "Financial Dashboard", href: "/admin/financial-dashboard" },
   ];
-  // const { data: user } = useUser();
+  const { data: user } = useUser();
 
-  // const hasPerms = () => {
-  //   console.log(user);
-  //   if (!user || !user.data) return false;
-  //   return user.data.role.perm_level > 5;
-  // };
+  const hasPerms = () => {
+    console.log(user);
+    if (!user || !user.data) return false;
+    return user.data.role.perm_level > 5;
+  };
 
-  // const hasAccess = hasPerms();
-  // if(!user) return <NotLoggedIn hasUser={false}/>;
-  // if(!hasAccess) return <NotLoggedIn hasUser={true}/>;
+  const hasAccess = hasPerms();
+  if(!user) return <NotLoggedIn hasUser={false}/>;
+  if(!hasAccess) return <NotLoggedIn hasUser={true}/>;
   return (
     <div className="min-h-screen bg-gray-100">
       <Navbar links={adminLinks} title="NEWRR Forms Dashboard" />
