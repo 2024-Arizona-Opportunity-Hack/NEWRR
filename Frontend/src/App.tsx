f280dcc016e2afe2018821356f15f16eeaa96021
Ronald McInnes <rjmcars8@gmail.com>
2024-10-12 21:54:18 -0700

    feat: added test functionality to upload images to s3 for unexpiring image links

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,5 +1,6 @@
 import Home from "./pages/homepage";
 import Admin from "./pages/admin";
+import Test from "./pages/test";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/admin" element={<Admin />} />
+        <Route path="/test" element={<Test />} />
       </Routes>
     </Router>
   );
